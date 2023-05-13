924a8a810cd8ec3bf18caa5467663aa8d36e4692
Md Asaduzaman <asadasad9282@gmail.com>
2023-05-13 17:40:26 +0600

    Create Model using Interface Schema and Insert Data

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { Schema, model } from 'mongoose'
 
@@ -19,88 +19,116 @@ app.use(express.urlencoded({ extended: true }))
  * Step4: Database Query
 */
 
-// Step1: Create Iterface
-interface IUser {
-    id: string;
-    role: "student"
-    password: string;
-    name: {
-        firstName: string;
-        lastName: string;
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+
+
+    // Step1: Create Iterface
+    interface IUser {
+        id: string;
+        role: "student"
+        password: string;
+        name: {
+            firstName: string;
+            middleName?: string,
+            lastName: string;
+        }
+        dateOfBirth?: string;
+        gender: "male" | "female";
+        email?: string;
+        contactNo: string;
+        emergencyContactNo: string;
+        presentAddres: string;
+        permanentAddress: string;
+
     }
-    dateOfBirth?: string;
-    gender: "male" | "female";
-    email?: string;
-    contactNo: string;
-    emergencyContactNo: string;
-    presentAddres: string;
-    permanentAddress: string;
 
-}
-
-// Creating Schema using interface
-const userSchema = new Schema<IUser>({
-    id: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    name: {
-        firstName: {
+    // Creating Schema using interface
+    const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        password: {
             type: String,
             required: true
         },
-        middlename: {
+        name: {
+            firstName: {
+                type: String,
+                required: true
+            },
+            middleName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+                required: true
+            }
+        },
+        dateOfBirth: {
             type: String,
             required: true
-
         },
-        lastName: {
+        gender: {
+            type: String,
+            enum: ["male", "female"],
+        },
+        email: {
+            type: String,
+        },
+        contactNo: {
+            type: String,
+            required: true
+        },
+        emergencyContactNo: {
+            type: String,
+            required: true
+        },
+        presentAddres: {
+            type: String,
+            required: true
+        },
+        permanentAddress: {
             type: String,
             required: true
         }
-    },
-    dateOfBirth: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String,
-        enum: ["male", "female"],
-    },
-    email: {
-        type: String,
-    },
-    contactNo: {
-        type: String,
-        required: true
-    },
-    emergencyContactNo: {
-        type: String,
-        required: true
-    },
-    presentAddres: {
-        type: String,
-        required: true
-    },
-    permanentAddress: {
-        type: String,
-        required: true
+
+    });
+
+    // 3. Create a Model.
+    const User = model<IUser>('User', userSchema);
+
+    const creaToUserToDb = async () => {
+
+        const user = new User({
+            id: "1111002",
+            role: "student",
+            password: "mypassword",
+            name: {
+                firstName: "Mr",
+                lastName: "Hanks",
+            },
+            dateOfBirth: '11, sep 2000',
+            gender: "male",
+            email: "asaduzzaman1156@gmail.com",
+            contactNo: "015888",
+            emergencyContactNo: "00000",
+            presentAddres: "kgc",
+            permanentAddress: "kgc",
+        });
+
+        await user.save();
+        console.log(user);
     }
 
-});
+    creaToUserToDb()
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
 })
 
 export default app;
